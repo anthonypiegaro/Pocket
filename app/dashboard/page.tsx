a9ef2fb5727e64dc9ac73ed9bbc0595aee542ef0
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2025-07-30 22:32:53 +0000

    Remove Waiting Func

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,6 @@ import { getPocketItems } from "./get-pocket-items"
 export default async function Dashboard() {
   const [pocketTags, pocketItems] = await Promise.all([getPocketTags(), getPocketItems()])
 
-  await new Promise(resolve => setTimeout(resolve, 30000))
   return (
     <DashboardWrapper 
       pocketItems={pocketItems}
